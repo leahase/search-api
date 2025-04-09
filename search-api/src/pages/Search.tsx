75333911025475c67538f93ea840d701094efaee
Lea Haselsteiner <leahaselsteiner@hotmail.com>
2025-04-09 18:50:15 +0200

    tog kod från mitt forra f;rs;ktet och anv;nde koden till det - d'rf;r så stora commits

--- search-api/src/pages/Search.tsx
+++ search-api/src/pages/Search.tsx
@@ -2,41 +2,24 @@ import axios from 'axios';
 import { ChangeEvent, FormEvent, useState } from "react";
 import { SearchResult } from "../models/Searchresult";
 
-<<<<<<< HEAD
-
 export const Search = () => {
     const [searchText, setSearchText] = useState<string>('');
     const [results, setResults] = useState<SearchResult[]>([]);
-    const [page, setPage] = useState<number>(1);
-    
 
     const handleSearch = async (e:FormEvent, requestedPage = 1) => {
         e.preventDefault()
-        setPage(requestedPage);
 
-        const startIndex = ( requestedPage - 1) * 10 + 1;
-=======
-export const Search = () => {
-    const [searchText, setSearchText] = useState<string>('');
-    const [results, setResults] = useState<SearchResult[]>([]);
+        const startIndex = (requestedPage - 1) * 10 + 1;
 
-    const handleSearch = async (e:FormEvent) => {
-        e.preventDefault()
->>>>>>> 57f53f24c263658f304dcb22ee28725db95e2b37
         try {
             const response = await axios.get('https://www.googleapis.com/customsearch/v1', {
             params: {
                 q: searchText,
-<<<<<<< HEAD
                 key: process.env.API_KEY,
                 cx: process.env.GOOGLE_CX,
                 start: startIndex,
             },
-=======
-                key: 'AIzaSyCAAwq8YB6nSyxv3fchhHufYqAalokqpyE',
-                cx: '26f67b9b7c5ac42e0',
             }
->>>>>>> 57f53f24c263658f304dcb22ee28725db95e2b37
           })
           setResults(response.data.items)
         } catch (error) {
